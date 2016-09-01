2e7e7fd4fa6d8f1b2b867749adcf50d588ec0f3c
vivsd <vivs.is.here@gmail.com>
2016-09-02 00:50:13 +0530

    Added speech synthesizer to describe the grades.

--- C# Fundamentals with Allen Scott/Module 1/The Grade Book/Program.cs
+++ C# Fundamentals with Allen Scott/Module 1/The Grade Book/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Text;
+using System.Speech;
 using System.Threading.Tasks;
+using System.Speech.Synthesis;
 
 namespace The_Grade_Book
 {
@@ -18,11 +20,19 @@ namespace The_Grade_Book
             book.AddGrade(79);
             book.AddGrade(96);
 
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+
             GradeStatistics status = new GradeStatistics();
             status = book.CompuStatistics();
             Console.WriteLine("Highest grade " + status.HighestGrade);
             Console.WriteLine("Average grade " + status.AverageGrade);
             Console.WriteLine("Lowest grade " + status.LowestGrade);
+
+            synth.Speak("Highest grade is " + status.HighestGrade.ToString());
+            synth.Speak("Average grade is" + status.AverageGrade.ToString());
+            synth.Speak("Lowest grade is " + status.LowestGrade.ToString());
+
+            //synth.Speak(status.HighestGrade.ToString());
         }
     }
 }
